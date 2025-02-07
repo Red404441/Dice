fe62d8939b5f638c3a2caf84b3bb637cb66e0570
Red404441 <socallaghan24@hcetss.ie>
2025-02-07 11:22:55 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onGesture(Gesture.Shake, function () {
+    Number2 = randint(1, 3)
+    if (Number2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Number2 == 2) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
+let Number2 = 0
