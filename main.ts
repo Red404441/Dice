3ec1665eef061438fa3491b413dbbaf5c7357e6c
Red404441 <Red40444@proton.me>
2026-01-12 15:08:55 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,51 @@
+let Number2 = 0
 input.onGesture(Gesture.Shake, function () {
     Number2 = randint(1, 3)
     if (Number2 == 1) {
         basic.showLeds(`
             . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
+            . . . . .
+            . . # # .
+            . . # # .
             . . . . .
             `)
     } else if (Number2 == 2) {
         basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . . . . .
+            . . . . .
+            # # . # #
+            # # . # #
+            . . . . .
+            `)
+    } else if (false) {
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            . . # # .
+            . . # # .
             `)
     } else {
         basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
+            # # . # #
+            # # . # #
+            . . . . .
             # # . # #
             # # . # #
             `)
     }
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . # . .
+        # # . # #
+        # # . # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
 })
-let Number2 = 0
